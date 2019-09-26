d225de646745d439b8ede95bc20bc9ac5a2aba06
Mitra Ardron <mitra@mitra.biz>
2019-09-26 16:43:13 +1000

    Code review, cleanup relinting

--- ArchiveFile.js
+++ ArchiveFile.js
@@ -1,154 +1,192 @@
-//require('babel-core/register')({ presets: ['env', 'react']}); // ES6 JS below!
 const {fetch_json, gatewayServer, gateway, formats, } = require( './Util');
 const prettierBytes = require( "prettier-bytes");
 const waterfall = require('async/waterfall');
 //const DwebTransports = require('@internetarchive/dweb-transports'); //Not "required" because available as window.DwebTransports by separate import
 
+/**
+ * Represents a single file, currently one that is in the item, but might create sub/super classes to handle other types
+ * of file e.g. images used in the UI
+ *
+ * Fields:
+ * metadata: metadata of item - (note will be a pointer into a Detail or Search's metadata so treat as read-only)
+ *
+ */
 class ArchiveFile {
-    /*
-    Represents a single file, currently one that is in the item, but might create sub/super classes to handle other types
-    of file e.g. images used in the UI
-
-    Fields:
-    metadata: metadata of item - (note will be a pointer into a Detail or Search's metadata so treat as read-only)
-    */
-
-    constructor({itemid = undefined, metadata = undefined}={}) {
-        this.itemid = itemid;
-        if (typeof metadata.downloaded !== "undefined") {
-            // Support dweb-mirror which stores downloaded as AF.metadata.downloaded but needs it as AF.downloaded
-            this.downloaded = metadata.downloaded;
-            delete(metadata.downloaded);
-        }
-        this.metadata = metadata;
+  constructor({itemid = undefined, metadata = undefined}={}) {
+    this.itemid = itemid;
+    if (typeof metadata.downloaded !== "undefined") {
+      // Support dweb-mirror which stores downloaded as AF.metadata.downloaded but needs it as AF.downloaded
+      this.downloaded = metadata.downloaded;
+      delete(metadata.downloaded);
     }
+    this.metadata = metadata;
+  }
 
-    static new({archiveitem=undefined, filename=undefined, copyDirectory=undefined}={}, cb) {
-        /*
-         Asynchronously create a new ArchiveFile instance and load its metadata.
-
-         archiveitem:   Instance of ArchiveItem with or without its metadata loaded
-         itemid:        Identifier of item (only used if archiveitem not defined)
-         metadata:      If defined is the result of a metadata API call for loading in AF.metadata
-         filename:      Name of an existing file, (may be multipart e.g. foo/bar)
-         copyDirectory  Where to store any data cached, (if running in DwebMirror)
-         cb(err, archivefile): passed Archive File
-         resolves to:   archivefile if no cb
-          errors:        FileNotFound or errors from ArchiveFile() or fetch_metadata()
-        */
-        if (cb) { return f.call(this, cb) } else { return new Promise((resolve, reject) => f.call(this, (err, res) => { if (err) {reject(err)} else {resolve(res)} }))}
-        function f(cb) {
-            if (!archiveitem.metadata) {
-                archiveitem.fetch_metadata({copyDirectory}, (err, ai) => { // Note will load from cache if available and load ai.metadata and ai.files
-                    if (err)  { cb(err) } else { this.new({archiveitem: ai, filename}, cb); } })
-            } else {
-                const af = archiveitem.files.find(af => af.metadata.name === filename); // af, (undefined if not found)
-                return af ? cb(null, af) : cb(new Error(`${archiveitem.itemid}/${filename} not found`));
-            }
-        }
-    };
-
-    name() {
-        /* Name suitable for downloading etc */
-        return this.metadata.name;
-    }
-
-    urls(cb) { //TODO-MIRROR fix this to make sense for _torrent.xml files which dont have sha1 and probably not IPFS
-        /*
-        cb(err, urls)   passed an array of urls that might be a good place to get this item
-        if no cb: resolve to urls
-        Throws: Error if fetch_json doesn't succeed, or retrieves something other than JSON
-         */
-        if (cb) { try { f.call(this, cb) } catch(err) { cb(err)}} else { return new Promise((resolve, reject) => { try { f.call(this, (err, res) => { if (err) {reject(err)} else {resolve(res)} })} catch(err) {reject(err)}})} // Promisify pattern v2
-        function f(cbout) {
-            waterfall([
-                (cb) => DwebTransports.p_connectedNames(cb),
-                (connectedNames, cb) => { // Decide if need to get file-specific metadata because missing dweb urls
-                    if  (  (!this.metadata.ipfs && connectedNames.includes("IPFS"))
-                        // TODO set magnetlink when build ArchiveFile based on it being on item, not needed here at all.
-                        || (!this.metadata.magnetlink && !(this.metadata.name === "__ia_thumb.jpg") && connectedNames.includes("WEBTORRENT")) // Want magnetlink, but not if its a thumbnail as causes too many webtorrent downloads
-                     // || !this.metadata.contenthash // Dont do another roundtrip just to get contenthash
-                        ) {   // Connected to IPFS but dont have IPFS URL yet (not included by default because IPFS caching is slow)
-                        let name = this.metadata.name.replace('?', '%3F');  // Fjords: 17BananasIGotThis/17 Bananas? I Got This!.mp3  has a '?' in it
-                        // TODO using fetch_json on server is ok, but it would be better to incorporate Gun & Wolk and go via DwebTransports
-                        // maybe problem offline but above test should catch cases where no IPFS so not useful
-                        fetch_json(`${gatewayServer()}${gateway.url_metadata}${this.itemid}/${encodeURIComponent(name)}`, (err, res)=>{
-                            if (!err) this.metadata = res;
-                            cb(err); });
-                    } else {
-                        cb(null);
-                    }},
-                (cb) => {
-                    const res = [this.metadata.ipfs, this.metadata.magnetlink, this.metadata.contenthash].filter(f => !!f);   // Multiple potential sources eliminate any empty
-                    res.push(this.httpUrl()); // HTTP link to file (note this was added Oct2018 and might not be correct)
-                    cb(null, res);
-                }
-                ], cbout);
-        }
-    }
-
-    httpUrl() {
-        // This will typically be dweb.me, but may be overridden un URL with mirror=localhost:4244
-        return `${gatewayServer()}${gateway.urlDownload}/${this.itemid}/${this.metadata.name}`;
-    }
-    mimetype() {
-        let f =  formats("format", this.metadata.format)
-        if (typeof f === "undefined") {
-            const ext = this.metadata.name.split('.').pop();
-            f =  formats("ext", "."+ext)
-        }
-        return  (typeof f === "undefined") ? undefined : f.mimetype;
-    }
-    data(cb) {
-      /**
-       * Fetch data, normally you should probably be streaming instead.
-       * Returns data via Promise or cb
-       * Errors TransportError (or poss CodingError) if urls empty or cant fetch (via Promise or cb)
-       * Not timed-out currently as only used in .blob which could be slow on big files
-       */
-      if (cb) { try { f.call(this, cb) } catch(err) { cb(err)}} else { return new Promise((resolve, reject) => { try { f.call(this, (err, res) => { if (err) {reject(err)} else {resolve(res)} })} catch(err) {reject(err)}})} // Promisify pattern v2
-      function f(cb) {
-        waterfall([
-            (cb) => this.urls(cb),
-            (res, cb2) => DwebTransports.fetch(res, {}, cb2),
-        ], cb)
+  /**
+   *
+   * @param archiveitem   Instance of ArchiveItem with or without its metadata loaded
+   * @param filename      Name of an existing file, (may be multipart e.g. foo/bar)
+   * @param copyDirectory Where to store any data cached, (if running in DwebMirror)
+   * @param cb(err, ArchiveFile)  errors: FileNotFound or errors from ArchiveFile or fetch_metadata()
+   * @returns {Promise<ArchiveFile>|undefined} if no cb
+   */
+  static new({archiveitem=undefined, filename=undefined, copyDirectory=undefined}={}, cb) {
+    if (cb) { return f.call(this, cb) } else { return new Promise((resolve, reject) => f.call(this, (err, res) => { if (err) {reject(err)} else {resolve(res)} }))}
+    function f(cb) {
+      if (!archiveitem.metadata) {
+        archiveitem.fetch_metadata({copyDirectory}, (err, ai) => { // Note will load from cache if available and load ai.metadata and ai.files
+          if (err)  { cb(err) } else { this.new({archiveitem: ai, filename}, cb); } })
+      } else {
+        const af = archiveitem.files.find(af => af.metadata.name === filename); // af, (undefined if not found)
+        return af ? cb(null, af) : cb(new Error(`${archiveitem.itemid}/${filename} not found`));
       }
     }
-    blobUrl(cb) {  // Not timedout currently as could be slow on big files
-        this.data((err, data) => {
-            if (err) {
-                cb(err);
-            } else {
-                cb(null, URL.createObjectURL(
-                  new Blob([data], {type: this.mimetype()})));
-            }
-        })
-    }
+  };
 
-    sizePretty() {
-        try {
-            return this.metadata.size
-              ? prettierBytes(parseInt(this.metadata.size))
-              : ""; // For example files.xml has no size field
-        } catch (err) {
-            debug("ERROR - cant get size for %s/%s", this.itemid, this.metadata.name)
-            return "";
+  /**
+   * Name suitable for downloading etc
+   * @returns string
+   */
+  name() {
+    return this.metadata.name;
+  }
+
+  /**
+   *
+   * @param cb(err, [URL])  Array of urls that might be a good place to get this item
+   * @returns {Promise<[URL]>} if no cb
+   * @errors if fetch_json doesn't succeed, or retrieves something other than JSON
+   */
+  urls(cb) { //TODO-MIRROR fix this to make sense for _torrent.xml files which dont have sha1 and probably not IPFS
+    if (cb) { try { f.call(this, cb) } catch(err) { cb(err)}} else { return new Promise((resolve, reject) => { try { f.call(this, (err, res) => { if (err) {reject(err)} else {resolve(res)} })} catch(err) {reject(err)}})} // Promisify pattern v2
+    function f(cbout) {
+      // noinspection JSUnresolvedFunction
+      waterfall([
+        (cb) => DwebTransports.p_connectedNames(cb),
+        (connectedNames, cb) => { // Decide if need to get file-specific metadata because missing dweb urls
+          if  (  (!this.metadata.ipfs && connectedNames.includes("IPFS"))
+            // TODO set magnetlink when build ArchiveFile based on it being on item, not needed here at all.
+            || (!this.metadata.magnetlink && !(this.metadata.name === "__ia_thumb.jpg") && connectedNames.includes("WEBTORRENT")) // Want magnetlink, but not if its a thumbnail as causes too many webtorrent downloads
+          // || !this.metadata.contenthash // Dont do another roundtrip just to get contenthash
+          ) {   // Connected to IPFS but dont have IPFS URL yet (not included by default because IPFS caching is slow)
+            let name = this.metadata.name.replace('?', '%3F');  // Fjords: 17BananasIGotThis/17 Bananas? I Got This!.mp3  has a '?' in it
+            // TODO using fetch_json on server is ok, but it would be better to incorporate Gun & Wolk and go via DwebTransports
+            // maybe problem offline but above test should catch cases where no IPFS so not useful
+            fetch_json(`${gatewayServer()}${gateway.url_metadata}${this.itemid}/${encodeURIComponent(name)}`, (err, res)=>{
+              if (!err) this.metadata = res;
+              cb(err); });
+          } else {
+            cb(null);
+          }},
+        (cb) => {
+          const res = [this.metadata.ipfs, this.metadata.magnetlink, this.metadata.contenthash].filter(f => !!f);   // Multiple potential sources eliminate any empty
+          res.push(this.httpUrl()); // HTTP link to file (note this was added Oct2018 and might not be correct)
+          cb(null, res);
         }
+      ], cbout);
     }
-    istype(type) {
-        // True if specify a type and it matches, or don't specify a type BUT fails if type unrecognized
-        let format = formats("format", this.metadata.format);
-        //if (!format) console.warn("Format", this.metadata.format, "unrecognized");
-        return format && (!type || (format.type === type));
+  }
+
+  /**
+   * @returns {URL} http URL - typically on dweb.me or localhost:4244
+   */
+  httpUrl() {
+    return `${gatewayServer()}${gateway.urlDownload}/${this.itemid}/${this.metadata.name}`;
+  }
+
+  /**
+   * @returns STRING  converted from metadata.format to a real mime type
+   */
+  mimetype() {
+    let f =  formats("format", this.metadata.format);
+    if (typeof f === "undefined") {
+      const ext = this.metadata.name.split('.').pop();
+      f =  formats("ext", "."+ext)
     }
-    // noinspection JSUnusedGlobalSymbols
-    playable(type) {
-        return this.istype(type) && formats("format", this.metadata.format).playable;
+    return  (typeof f === "undefined") ? undefined : f.mimetype;
+  }
+
+  // noinspection DuplicatedCode
+  /**
+   *
+   * Fetch data, normally you should probably be streaming instead.
+   * Not timed-out currently as only used in .blob which could be slow on big files
+   *
+   * @param cb(err, buffer as returned by DwebTransports.fetch)
+   * @returns {Promise<buffer>} if no cb
+   * @errors TransportError if urls empty or cant fetch;
+   *
+   */
+  data(cb) {
+    if (cb) { try { f.call(this, cb) } catch(err) { cb(err)}} else { return new Promise((resolve, reject) => { try { f.call(this, (err, res) => { if (err) {reject(err)} else {resolve(res)} })} catch(err) {reject(err)}})} // Promisify pattern v2
+    function f(cb) {
+      waterfall([
+        (cb) => this.urls(cb),
+        (res, cb2) => DwebTransports.fetch(res, {}, cb2),
+      ], cb)
     }
-    // noinspection JSUnusedGlobalSymbols
-    downloadable(type) {
-        return this.istype(type) && !!formats("format", this.metadata.format).downloadable;
+  }
+
+  /**
+   *  Not timedout currently as could be slow on big files
+   * @param cb(err, URL) Url of blob
+   * @errors see data()
+   */
+  blobUrl(cb) {  //
+    this.data((err, data) => {
+      if (err) {
+        cb(err);
+      } else {
+        cb(null, URL.createObjectURL(
+          new Blob([data], {type: this.mimetype()})));
+      }
+    })
+  }
+
+  /**
+   * @returns {string} of the size suitable for pretty printing or ""
+   */
+  sizePretty() {
+    try {
+      return this.metadata.size
+        ? prettierBytes(parseInt(this.metadata.size))
+        : ""; // For example files.xml has no size field
+    } catch (err) {
+      debug("ERROR - cant get size for %s/%s", this.itemid, this.metadata.name);
+      return "";
     }
+  }
+
+  /**
+   *
+   * @param type        From range in format field of util._formatarr e.g. "PDF" or "JPEG Thumb"
+   * @returns {boolean} True if file is of appropriate type
+   */
+  istype(type) {
+    // True if specify a type and it matches, or don't specify a type BUT fails if type unrecognized
+    let format = formats("format", this.metadata.format);
+    //if (!format) console.warn("Format", this.metadata.format, "unrecognized");
+    return format && (!type || (format.type === type));
+  }
+
+  /**
+   * @param type  as in format field of _formatarr
+   * @returns {boolean} if can be played (based on _formatarr)
+   */
+  // noinspection JSUnusedGlobalSymbols
+  playable(type) {
+    return this.istype(type) && formats("format", this.metadata.format).playable;
+  }
+
+  /**
+   * @param type as in format field of _formatarr
+   * @returns {boolean} true if downloadable
+   */
+  downloadable(type) {
+    return this.istype(type) && !!formats("format", this.metadata.format).downloadable;
+  }
 
 }
 exports = module.exports = ArchiveFile;
+
+//Code Inspection 2019-09-26

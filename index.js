ea3b0ab0cead38847d0169badd8120d2eb7f7238
Mitra Ardron <mitra@mitra.biz>
2019-11-12 14:06:06 +1100

    Add an index file

---
+++ index.js
@@ -0,0 +1,15 @@
+/**
+ * Export all of the public components here
+ */
+
+const ArchiveFile = require('./ArchiveFile');
+const ArchiveItem = require('./ArchiveItem');
+const ArchiveMember = require('./ArchiveMember');
+const RawBookReaderResponse = require('./RawBookReaderResponse');
+// Add specific cases here as used elsewhere.
+const { torrentConfigDefault, dwebTorrentObjectFrom } = require('./mungeTorrent');
+//const { } = require('./Util');
+
+exports = module.exports = { ArchiveFile, ArchiveItem, ArchiveMember, RawBookReaderResponse,
+  torrentConfigDefault, dwebTorrentObjectFrom
+}

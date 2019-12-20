63bdeed11a553a9a22250d194ef4f100a373c8c3
Mitra Ardron <mitra@mitra.biz>
2019-12-21 07:20:21 +1100

    RawBookReaderJSONResponse to support BookReaderJSON API that ArchiveLab has

--- RawBookReaderJSONResponse.js
+++ RawBookReaderJSONResponse.js
@@ -1,48 +1,66 @@
-const debug = require('debug')('RawBookReaderResponse');
+// const debug = require('debug')('RawBookReaderJSONResponse');
+const { parmsFrom } = require('./Util.js');
 
-class RawBookReaderResponse {
-    /*
-    Exactly the data structure returned from BookReaderJSIA.php
-    { data { data, brOptions, lendingInfo, metadata }
-    TODO-IAUX this is intended to match equivalent class in IAUX
-    TODO - refactor to use this
-     */
-    constructor(props) {
-        // Create a new API from props, using a shallow copy (may switch to deeper copy if reqd)
-        //[ "data", "brOptions", "lendingInfo", "metadata"].forEach( k => this[k] = props[k]);
-        this.data = props.data;
-    }
+class RawBookReaderJSONResponse {
+  /*
+  Subset of the data structure returned from BookReaderJSON.php
 
-    static fromArchiveItem(from) {
-        // Create a RawBookReaderResponse from a ArchiveItem (typically to then be exported)
-        return new this({ data: Object.assign({}, from.bookreader, {metadata: from.metadata})});
-    }
-    cooked({server=undefined, protocol=undefined}={}) {
-        // Cook the results of the API for passing to browser (with or without mirror) - alternative to asking for '.data"
-        // TODO-BOOK This could be running in different places, document where here.
+  See teh similar RawBookReaderResponse.js
+  */
+  constructor(props) {
+    // Create a new API from props, using a shallow copy (may switch to deeper copy if reqd)
+    this.data = props.data;
+  }
 
-        /* Typical no-mirror scenario
-            browser calls fetch_bookreader which forwards to datanode
-            datanode returns with server=DATANODE and urls https://DATANODE...
-            Browser uses this url to request page from DATANODE
-         */
-        /* Typical mirror scenario
-            browser calls fetch_bookreader which forwards to localhost with server=localhost:4244
-            Mirror catches this, calls fetch_bookreader which fetches from datanode or dweb.archive.org with server=DATANODE
-            datanode returns with server=DATANODE and urls https://DATANODE... which is what gets cached
-            Mirror cooks (in mirrorHttp) based on browser's server= when returning to browser
-            Browser uses cooked url to request page from mirror
-         */
+  /**
+   *
+   * @param from    ArchiveItem { itemid, metadata, server, bookreader: {data, brOptions}}
+   * @param server  Optional override of server representing data node
+   * @param protocol Optional override of 'https'
+   * @returns {{server, archiveFormat: *, titleIndex: number, collection: (*|string), pageNums, title: *, pageHeights, url: string, titleImage: string, itemId: *, numPages, subPrefix, itemPath: *, pageWidths, leafNums: *, titleLeaf: string, previewImage: string}}
+   */
+  static fromArchiveItem(from, { server = undefined, protocol = undefined } = {}) {
+    // Create a RawBookReaderResponse from a ArchiveItem (typically to then be exported)
+    if (typeof server === 'undefined') server = from.server;
+    if (typeof protocol === 'undefined') protocol = 'https';
 
-        if (server) {
-            const oldServer = this.data.brOptions.server;
-            this.data.brOptions.data.forEach(d1 => d1.forEach(d2 => d2.uri = d2.uri.replace(oldServer, server)))
-            //TODO-BOOK need to edit https to http
-        }
-        if (protocol) {
-            this.data.brOptions.data.forEach(d1 => d1.forEach(d2 => d2.uri = d2.uri.replace("https", protocol)))
-        }
-        return this.data;
-    }
+    const jsia = from.bookreader;
+    const metadata = from.metadata;
+    const brOptions = jsia.brOptions;
+    const leafs = [].concat(...brOptions.data); // flatten
+    if (typeof server === 'undefined') server = from.server;
+    const subPrefix = brOptions.subPrefix;
+    const previewUrlParms = {
+      id: metadata.identifier,
+      subPrefix,
+      itemPath: brOptions.bookPath,
+      server
+    };
+    const previewUrlStart = `${protocol}://${server}/BookReader/BookReaderPreview.php?${parmsFrom(previewUrlParms)}&`;
+    const res = {
+      archiveFormat: brOptions.zip ? 'zip' : undefined, // Dont have any examples currently where its not zip
+      collection: metadata.collection[0], // Always set for a book
+      itemId: metadata.identifier,
+      itemPath: brOptions.bookPath,
+      leafNums: leafs.map(l => l.leafNum),
+      numPages: leafs.length,
+      pageHeights: leafs.map(l => l.height),
+      pageWidths: leafs.map(l => l.width),
+      pageNums: leafs.map(unusedL => ''), //TODO find an example that has pageNums != leafNums or something other than ""
+      previewImage: previewUrlStart + 'page=preview&',
+      server, // localhost:4244 or ia123456.us.archive.org
+      subPrefix,
+      title: brOptions.bookTitle, // Also at metadata.title
+      titleImage: previewUrlStart + 'page=title&',
+      titleIndex: 0, // TODO find an example where this is not true
+      titleLeaf: '0', // TODO find an example where this is not true
+      url: `${protocol}://${server}/${jsia.data.bookUrl}`, // e.g. http://localhost:4244/details/foo or https://ia123456/details/foo
+    };
+    ['date', 'language', 'ppi', 'publisher'].forEach(k => res[k] = metadata[k]);
+    ['imageFormat', 'pageProgression', 'zip'].forEach(k => res[k] = brOptions[k]);
+    return res;
+  }
 }
-exports = module.exports = RawBookReaderResponse;
+exports = module.exports = RawBookReaderJSONResponse;
+
+// Code Inspection Mitra 2019-12-17
--- index.js
+++ index.js
@@ -6,10 +6,12 @@ const ArchiveFile = require('./ArchiveFile');
 const ArchiveItem = require('./ArchiveItem');
 const ArchiveMember = require('./ArchiveMember');
 const RawBookReaderResponse = require('./RawBookReaderResponse');
+const RawBookReaderJSONResponse = require('./RawBookReaderJSONResponse');
 // Add specific cases here as used elsewhere.
 const { torrentConfigDefault, dwebMagnetLinkFrom, dwebTorrentObjectFrom } = require('./mungeTorrent');
 const { formats } = require('./Util');
 
-exports = module.exports = { ArchiveFile, ArchiveItem, ArchiveMember, RawBookReaderResponse,
+exports = module.exports = { ArchiveFile, ArchiveItem, ArchiveMember, RawBookReaderResponse, RawBookReaderJSONResponse,
   dwebMagnetLinkFrom, dwebTorrentObjectFrom, formats, torrentConfigDefault
-}
+};
+// Code review 2019-12-17 by Mitra
